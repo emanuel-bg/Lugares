e4abfd86027e853e15eca6e6a5135908b43c5058
emanuel-bg <emmanuelbg30@gmail.com>
2022-06-17 14:12:57 -0600

    Semana 4 no funcionaba y ahora si, y Semana 5

---
+++ Lugares/app/src/main/java/com/lugares_v/ui/lugar/AddLugarFragment.kt
@@ -0,0 +1,93 @@
+package com.lugares_v.ui.lugar
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.lugares_v.R
+import com.lugares_v.databinding.FragmentAddLugarBinding
+import com.lugares_v.databinding.FragmentLugarBinding
+import com.lugares_v.model.Lugar
+import com.lugares_v.viewmodel.LugarViewModel
+
+class AddLugarFragment : Fragment() {
+    private var _binding: FragmentAddLugarBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var lugarViewModel: LugarViewModel
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        lugarViewModel =
+            ViewModelProvider(this)[LugarViewModel::class.java]
+        _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
+        binding.btAgregar.setOnClickListener {
+            addLugar()
+        }
+        ubicaGPS()
+        return binding.root
+    }
+
+    private fun ubicaGPS() {
+        val ubicacion: FusedLocationProviderClient=
+            LocationServices.getFusedLocationProviderClient(requireContext())
+
+        if(ActivityCompat.checkSelfPermission(requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            //Si no tengo los permisos los solicito
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),105)
+        }else
+        {
+            ubicacion.lastLocation.addOnSuccessListener { location:Location? ->
+                if (location!=null){
+
+                    binding.tvLatitud.text="${location.latitude}"
+                    binding.tvLongitud.text="${location.longitude}"
+                    binding.tvAltura.text="${location.altitude}"
+                }else
+                {
+                    binding.tvLatitud.text="0.00"
+                    binding.tvLongitud.text="0.00"
+                    binding.tvAltura.text="0.00"
+                }
+            }
+
+        }
+    }
+
+
+    private fun addLugar() {
+        val nombre=binding.etNombre.text.toString()
+        val correo=binding.etCorreo.text.toString()
+        val telefono=binding.etTelefono.text.toString()
+        val web=binding.etWeb.text.toString()
+        val latitud=binding.tvLatitud.text.toString().toDouble()
+        val longitud=binding.tvLongitud.text.toString().toDouble()
+        val altura=binding.tvAltura.text.toString().toDouble()
+        if (nombre.isNotEmpty()) { //Si puedo crear un lugar
+            val lugar= Lugar(0,nombre,correo,telefono,web,latitud, longitud,altura,"","")
+            lugarViewModel.addLugar(lugar)
+            Toast.makeText(requireContext(),getString(R.string.msg_lugar_added),Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addLugarFragment_to_nav_lugar)
+        } else {  //Mensaje de error...
+            Toast.makeText(requireContext(),getString(R.string.msg_data),Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
